4c8100912816c98c41c9d88869c884873c49863a
kongkham-lk <kongkham.luangkhot@gmail.com>
2023-06-18 19:51:12 -0700

    initial add form validation
    after add Ract-Hook-Form to this react form, Cannot type anything in form's input box

--- exercise combined/src/ReactForm/ShoppingListForm.jsx
+++ exercise combined/src/ReactForm/ShoppingListForm.jsx
@@ -1,40 +1,76 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 function ShoppingListForm({ addItem }) {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors }
+  } = useForm();
   const [formData, setFormData] = useState({ product: "", quantity: 0 });
+
   const handleChange = (e) => {
     setFormData((newItem) => {
-      return { 
-        ...newItem, 
-        [e.target.name]: e.target.value 
+      return {
+        ...newItem,
+        [e.target.name]: e.target.value
       };
     });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const onSubmit = (e) => {
+    // e.preventDefault();
+    console.log(formData);
     addItem(formData);
     setFormData({ product: "", quantity: 0 });
   };
+
+  const registerOptions = {
+    product: {
+      require: true,
+      message: "Product name cannot be empty"
+    },
+    quantity: {
+      require: true,
+      min: {
+        value: 1,
+        message: "Purchase item must more than 0"
+      },
+      max: {
+        value: 10,
+        message: "Cannot purchase more than 10 itmes"
+      }
+    }
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <label htmlFor="product">Product: </label>
       <input
         type="text"
         name="product"
         id="product"
         placeholder="Product Name"
-        onChange={handleChange}
         value={formData.product}
+        onChange={handleChange}
+        {...register("product", registerOptions.product)}
       />
+      <span className="text-danger">
+        {errors.product && errors.product.message}
+      </span>
       <label htmlFor="quantity">Quantity: </label>
       <input
         type="number"
         name="quantity"
         id="quantity"
         placeholder={formData.quantity}
-        onChange={handleChange}
         value={formData.quantity}
+        onChange={handleChange}
+        {...register("quantity", registerOptions.quantity)}
       />
+      <span className="text-danger">
+        {errors.quantity && errors.quantity.message}
+      </span>
       <button>Add Item</button>
     </form>
   );
